fbbd7b8b6104160238b8c267e7d5b5517cacf9b0
SARANYA251096 <saranyaayyamuthu96@gmail.com>
2023-11-19 21:38:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 6000;
+const db = require("./db/connections");
+const { userRouter } = require("./routes/user.routes");
+const { noteRouter } = require("./routes/note.routes");
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use("/user", userRouter)
+app.use("/note", noteRouter);
+
+
+
+// DAtabase
+db()
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Everyone! This is my notes app");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
